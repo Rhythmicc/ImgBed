1980e7f5699e01f46b76fa9eb65c385a1c7cc074
Rhythmicc <RhythmLian@outlook.com>
2023-01-14 23:04:29 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-VERSION = "0.0.0"
+VERSION = "0.0.1"
 
 setup(
     name="ImgBed",
@@ -25,7 +25,7 @@ setup(
     install_requires=["Qpro", "QuickStart_Rhy"],
     entry_points={
         "console_scripts": [
-            "ImgBed = ImgBed.main:main",
+            "img-bed = ImgBed.main:main",
         ]
     },
 )
